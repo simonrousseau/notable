1d5d0104450902afc3cc3789f3c0946c986360cc
Simon Rousseau <simonrousseau22@gmail.com>
2018-08-23 22:07:41 +0200

    Création des routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('./routes');
 
 module.exports = function(app, db) {
+
     routes(app, db);
 
 };
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,59 @@
 module.exports = function(app, db) {
-    app.post('/notes', (req, res) => {
-        console.log(req.body)
-        res.send('Hello')
+
+    var ObjectID = require('mongodb').ObjectID;
+
+    const collection = 
+
+    // Lecture d'une note
+    app.get('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      db.collection('notes').findOne(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send(item);
+        }
+      });
     });
-};
+
+    // Création d'une note
+    app.post('/notes', (req, res) => {
+      const note = { text: req.body.body, title: req.body.title };
+      db.collection('notes').insert(note, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+    });
+
+    // Suppression d'une note
+    app.delete('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      db.collection('notes').remove(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send('Note ' + id + ' deleted!');
+        }
+      });
+    });
+
+    // Mise à jour d'une note
+    app.put('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      const note = { text: req.body.body, title: req.body.title };
+      db.collection('notes').update(details, note, (err, result) => {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            res.send(note);
+        }
+      });
+    });
+
+  };
